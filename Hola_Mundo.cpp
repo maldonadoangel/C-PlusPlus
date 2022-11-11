faf839f12b15b583fc51193c4aec684edb862926
Angel Morales <maldonadoangel7069@gmail.com>
2022-11-11 06:05:48 -0600

    Uso de unsigned en numeros enteros

--- Hola_Mundo.cpp
+++ Hola_Mundo.cpp
@@ -4,10 +4,17 @@ using namespace std;
 
 int main(){
     string nombre;
+    //unsigned nos permite solo almacenar valores positivos, eliminando todo valor negativo
+    //tambien nos duplica el espacio del numero aceptado, unsigned lo podemos utilizar en
+    //todo tipo de variables enteras 
+    unsigned short edad;
     cout<<"Hola Mundo"<<endl;
     cout<<"Ingresa tu nombre"<<endl;
     cin>>nombre;
     cout<<"Bienvenido: " + nombre<<endl;
+    cout<<"Ingresa tu edad: ";
+    cin>>edad;
+    cout<<"Tu edad es de: " + edad<<endl;
     return 0;
     system("pause");
 }
