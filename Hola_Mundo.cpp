24a9f6fcad0bb8b2dcb0bdc7bfe5cd51a718fb9c
Angel Morales <maldonadoangel7069@gmail.com>
2022-11-11 05:23:57 -0600

    Hola Mundo en c++

---
+++ Hola_Mundo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string nombre;
+    cout<<"Hola Mundo"<<endl;
+    cout<<"Ingresa tu nombre"<<endl;
+    cin>>nombre;
+    cout<<"Bienvenido: " + nombre<<endl;
+    return 0;
+    system("pause");
+}
+    
+    
